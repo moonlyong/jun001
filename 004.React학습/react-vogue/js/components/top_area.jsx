a517fbec01df8a21b6316f46a21892f944c9e04f
moonlyong <140685725+moonlyong@users.noreply.github.com>
2023-11-06 18:20:03 +0900

    asd
    asd

--- 004.React학습/react-vogue/js/components/top_area.jsx
+++ 004.React학습/react-vogue/js/components/top_area.jsx
@@ -7,10 +7,12 @@ import { makeLink } from "./linksys2.js";
   컴포넌트명 : TopArea
   기능 : 상단영역 메뉴, 로그 등 요소 구성
 *******************************************/
-export default function TopArea() {
+export default function TopArea(props) {
   // 컴포넌트 요소 랜더링 직전 호출구역
-  // -> 컴포넌트는 모두 만들어진 후 화면뿌리기 직전(가랜더랭)
-  React.useLayoutEffect(makeLink);
+  // -> 컴포넌트는 모두 만들어진 후 화면뿌리기 직전(가랜더링)
+  React.useEffect(makeLink,[]);
+  // useEffect(함수,[]) -> 뒤에 의존성변수 구역 비어있으면
+  // 본 컴포넌트가 처음 실행될때 한번만 실행한다!
 
   // GNB용 메뉴 배열변수
   const gnbText = [
@@ -27,6 +29,8 @@ export default function TopArea() {
   // 메뉴 클릭시 변수 변경함수
   const chgCat = (data) => {
     console.log("나야나!", data);
+    // 전달된 부모함수 chgCat을 호출함!
+    props.chgItem(data.toLowerCase());
   }; ///////////// chgCat 함수 ///////////
 
   return (
